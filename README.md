d319876ae0dba1d58cc54c713a5792065ad5581e
Jefferson Leonata <leonatajeff@gmail.com>
2024-02-28 17:02:28 -0500

    Add GET API endpoint for history
    Retrieving past documents

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Work in Progress
-
-Live previews of the latest branch: https://sincerity-j3n5.vercel.app/
--- sincerity/src/app/api/history/route.tsx
+++ sincerity/src/app/api/history/route.tsx
@@ -1,4 +1,22 @@
+import {
+  DynamoDBClient,
+  GetItemCommand
+} from '@aws-sdk/client-dynamodb';
+
+const client = new DynamoDBClient({});
+
 export async function GET(request: Request) {
-  const res = await request.json()
-  return Response.json({ res })
+  const { searchParams } = new URL(request.url)
+  const id: string | null = searchParams.get('id')
+  const { Item } = await client.send(
+    new GetItemCommand({
+      TableName: process.env.TABLE_NAME,
+      Key: {
+        userid: { S: id ?? '' }
+      },
+      ProjectionExpression: 'docs'
+    })
+  );
+
+  return Response.json({ Item });
 }
