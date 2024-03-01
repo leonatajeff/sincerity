241f962e85d212554bf1910aa0293a54b4b76534
Jefferson Leonata <leonatajeff@gmail.com>
2024-03-01 14:55:57 -0500

    Update dashboard UI

--- sincerity/src/app/pages/dashboard/page.tsx
+++ sincerity/src/app/pages/dashboard/page.tsx
@@ -2,15 +2,15 @@ import Link from 'next/link'
 
 export default function Dashboard() {
   return (
-    <div>
-        <h1>Dashboard</h1>
-        <p>Welcome</p>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex flex-col gap-4">
         <Link href="/pages/dashboard/coverletter">
-            cover letter
+          <div className="py-2 px-4 bg-blue-500 text-white text-center rounded-md hover:bg-blue-600 transition-colors duration-300 cursor-pointer">Create a Cover Letter</div>
         </Link>
         <Link href="/pages/dashboard/history">
-            history
+          <div className="py-2 px-4 text-white text-center rounded-md hover:bg-gray-600 transition-colors duration-300 cursor-pointer">History</div>
         </Link>
+      </div>
     </div>
   )
 }
